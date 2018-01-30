fb562ab4607fbfc40fdf1724c565184dcb548a3f
Stepan Sendun <Stepan_Sendun@epam.com>
2018-01-30 11:21:40 +0200

    singleton created

--- index.js
+++ index.js
@@ -1,52 +1,28 @@
-class Car {
-    constructor(wheels, engine, transmission) {
-        this.wheels = wheels;
-        this.engine = engine;
-        this.transmission = transmission;
+const mySingleton = (function () {
+    let instance;
+
+    function init() {
+
+        const privateVariable = `I'm private variable`;
+
+        function privateMathod() {
+            console.log('private method')
+        }
+
+        function publicMethod() {
+            console.log('public method');
+        }
+
+        return {
+            publicMethod
+        }
     }
 
-    startEngine() {
-        console.log('ron don don');
+    return {
+        getInstance: function () {
+           return instance = instance ? instance : init();
+        }
     }
-}
+})();
 
-class Porshe extends Car {
-    constructor(wheels = 4, engine = '5.7 litre 612 hp V10', transmission = 'mechanic') {
-        super(wheels, engine, transmission);
-    }
 
-    startEngine() {
-        console.log('Porshe engine started');
-    }
-}
-
-class Toyota extends Car {
-    constructor(wheels = 4, engine = '3.5 L (3,470 cc) 1PZ', transmission = 'auto') {
-        super(wheels, engine, transmission);
-    }
-
-    startEngine() {
-        console.log('Toyota engine started');
-    }
-}
-
-class Kamaz extends Car {
-    constructor(wheels = 6, engine = 'super powerful kamaz engine', transmission = 'mechanic') {
-        super(wheels, engine, transmission);
-    }
-}
-
-function carFactory(typeOfCar) {
-    typeOfCar = typeOfCar.toLowerCase();
-    switch (typeOfCar){
-        case 'porshe':
-            return new Porshe();
-        case 'toyota':
-            return new Toyota();
-        case 'kamaz':
-            return new Kamaz();
-        default:
-            console.log(`We haven't developed that kind of car yet`);
-            break;
-    }
-}

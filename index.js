ae185f8a531e2652fec857482ae2aba0b44d9bf7
Stepan Sendun <Stepan_Sendun@epam.com>
2018-01-30 11:06:34 +0200

    car factory created

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
-const MySuperModule = (function () {
-    // Private variable
-
-    const userData = [];
-
-    // public methods
-    const sayHello = function (name) {
-        console.log(`Hello ${name}!`);
-    };
-
-    const storeUserData = function (data) {
-        return userData.push(data);
-    };
-
-    const getUserData = function () {
-        return userData;
-    };
-
-    return {
-        sayHello,
-        storeUserData,
-        getUserData
+class Car {
+    constructor(wheels, engine, transmission) {
+        this.wheels = wheels;
+        this.engine = engine;
+        this.transmission = transmission;
     }
-})();
+
+    startEngine() {
+        console.log('ron don don');
+    }
+}
+
+class Porshe extends Car {
+    constructor(wheels = 4, engine = '5.7 litre 612 hp V10', transmission = 'mechanic') {
+        super(wheels, engine, transmission);
+    }
+
+    startEngine() {
+        console.log('Porshe engine started');
+    }
+}
+
+class Toyota extends Car {
+    constructor(wheels = 4, engine = '3.5 L (3,470 cc) 1PZ', transmission = 'auto') {
+        super(wheels, engine, transmission);
+    }
+
+    startEngine() {
+        console.log('Toyota engine started');
+    }
+}
+
+class Kamaz extends Car {
+    constructor(wheels = 6, engine = 'super powerful kamaz engine', transmission = 'mechanic') {
+        super(wheels, engine, transmission);
+    }
+}
+
+function carFactory(typeOfCar) {
+    typeOfCar = typeOfCar.toLowerCase();
+    switch (typeOfCar){
+        case 'porshe':
+            return new Porshe();
+        case 'toyota':
+            return new Toyota();
+        case 'kamaz':
+            return new Kamaz();
+        default:
+            console.log(`We haven't developed that kind of car yet`);
+            break;
+    }
+}
